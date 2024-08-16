3199817e9c385949d3ec4f966b632880ec0c92ff
karensemanate <karen.semanate@soy.sena.edu.co>
2024-08-16 10:36:03 -0500

    cargando django con dos templates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class persona(models.model):
+    
+    ESTADO_CHOICES = [
+        ('activo', 'activo'),
+        ('inactivo','inactivo')
+    ]
+    name = models.CharField(max_length=20)
+    cedula = models.CharField(max_length=100)
+    telofono = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100, choices=ESTADO_CHOICES, default='activo')
+    datacreate = models.DateFiel(auto_now=True)
